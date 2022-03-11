04c264a08809e68d746b371180dfe2108c34fe56
harry.turner <hcg.turner@btinternet.com>
2022-03-11 00:57:42 +0000

    l_s_f 1.1

--- least_squares_fit.py
+++ least_squares_fit.py
@@ -20,15 +20,40 @@ def con_sec2(x, a, b, c, d, e, f):
 
 ## Inputs: xdata (must be np.array), ydata
 ## Output: ydata fitted to full dim x
-def fit(xdata, ydata):
-    popt, pcov = curve_fit(con_sec, xdata, ydata)
-#print(popt)
+## Option to return parameters. coeffs will be returned in "param_convert" form
+def fit(xdata, ydata, p0=[1000, 0, 10**5, 0, 0, 0], paramreturn=False):
+    popt, pcov = curve_fit(con_sec2, xdata, ydata, p0=p0)
+    print(popt)
+#print('hello', type(popt))
+    popt2, pcov2 = curve_fit(con_sec2, xdata, ydata, p0=popt)
+    print(popt2)
+    #popt3, pcov3 = curve_fit(con_sec2, xdata, ydata, p0=np.append(popt2, -0.05))
+#print(popt3, '\n', '\n')
+    if paramreturn == True:
+        return con_sec2(np.arange(main.dim), *popt2), param_convert(*popt2)
+    return con_sec2(np.arange(main.dim), *popt2)
+
+def simpfit(xdata, ydata, p0=[1000, 0, 10**5, 0, 0, 0], paramreturn=False):
+    popt, pcov = curve_fit(con_sec, xdata, ydata, p0=p0[:5])
+    print(popt)
 #print('hello', type(popt))
     popt2, pcov2 = curve_fit(con_sec, xdata, ydata, p0=popt)
-#print(popt2)
+    print(popt2)
     popt3, pcov3 = curve_fit(con_sec2, xdata, ydata, p0=np.append(popt2, -0.05))
-#print(popt3, '\n', '\n')
+    print(popt3, '\n', '\n')
+    if paramreturn == True:
+        return con_sec2(np.arange(main.dim), *popt3), param_convert(*popt2)
     return con_sec2(np.arange(main.dim), *popt3)
+
+## Input paramaters used in con_sec2
+## Output coeffs: y^2 + Ax^2 + By + Cx + Dxy + E = 0
+def param_convert(a, b, c, d, e, f=0):
+    A = f**2 - (b**2)*d
+    B = -2*a
+    C = 2*a*f - (b**2)*e
+    D = -2*f
+    E = a**2 - (b**2)*c
+    return A, B, C, D, E
 
 def self_function():
     image_data = UI.function()
@@ -37,9 +62,15 @@ def self_function():
 
     ##Inputs: data; square size; number of squares per row; x_min; x_max; continuous True or False; square True or False
     line_data, lineX, lineY = two_line_fit.find_peaks(sum_data, 32, 1, 1380, 1550, True, True)
-    xdata = np.array(lineX[0])
+    xdata = np.array(np.delete(lineX, 0))
     #xdata = xdata - 890
-    ydata = lineY[0]
+    ydata = lineY.pop(0)
+
+    curvey = fit(xdata, ydata)
+
+    plt.plot(xdata, ydata)
+    plt.plot(np.arange(main.dim), curvey)
+    plt.show()
 
     #main.plot(lineY[0], 'title', lineX[0])
     #main.plot(line_data, 'sum data square = True')
@@ -57,3 +88,5 @@ def self_function():
     plt.plot(xdata, con_sec2(xdata, *popt3), label='fit3')
     plt.legend()
     plt.show()
+
+#self_function()
