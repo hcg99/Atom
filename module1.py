7d3c2ca574f523e1738e86807c0942f3ea46f3d1
harry.turner <hcg.turner@btinternet.com>
2022-04-13 12:00:05 +0100

    0.3

--- module1.py
+++ module1.py
@@ -7,13 +7,12 @@ import scipy.constants as con
 T = 150
 T = T*con.e/con.k
 print(T)
-lam_, E_, y_ = bre.brem(bre.A, bre.B, T, bre.n*100)
+lam_, E_, y_ = bre.brem(T, bre.n, bre.A, bre.B)
 main.plot1D(y_, E_)
-x = input('hello')
 
 ## E: 2048 energies spanning full range (1100 - 1600 eV)
 ## rel_int: relative intensity of X-rays as a function of energy
-E, rel_int = bre.total(T=10**5, n=10**21)
+E, rel_int = bre.total(T, n=5*10**14)
 
 main.plot1D(rel_int, E, style='r--')
 
@@ -21,6 +20,7 @@ probability = rel_int / max(rel_int)
 
 main.plot1D(probability, E, style='r--')
 
+# creates array of positions of photon events
 def make_photons(prob):
     prob = prob / 50
     photons = np.zeros((main.dim, main.dim))
@@ -44,7 +44,7 @@ def make_photons(prob):
     return photons, count, multicount, pixhit, photonsflat
 
 photons, count, multicount, pixels, phoflat = make_photons(probability)
-print(count, 'phtons')
+print(count, 'photons')
 print('spanning', pixels, 'pixels')
 print(multicount, 'of which where in multi photon events')
 print(np.max(photons), 'is the most photons on 1 pixel')
